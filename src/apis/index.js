fe6e04b7a1ebfc759332c47064bc514702ecbd0a
Kazushi Isogai <kazu150@gmail.com>
2020-12-20 10:42:35 +0900

    contextとreducerでglobal stateを設定

---
+++ src/apis/index.js
@@ -0,0 +1,20 @@
+import axios from 'axios';
+
+const KEY = 'AIzaSyBosrvZbJDb28FtdhfVuMOfxdouH26yPK8';
+
+const youtube = axios.create({
+    baseURL: 'https://www.googleapis.com/youtube/v3'
+});
+
+export const fetchPopularData = async () => {
+    return await youtube.get('/videos', {
+        params: {
+            part: 'snippet',
+            maxResult: 40,
+            key: KEY,
+            regionCode: 'JP',
+            type: 'video',
+            chart: 'mostPopular'
+        }
+    })
+}
---
+++ src/pages/Top.js
@@ -0,0 +1,24 @@
+import React, { useEffect, useContext } from 'react';
+import Layout from '../components/Layout/Layout';
+import { fetchPopularData } from '../apis/index';
+import { Store } from '../store/index'
+
+const Top = () => {
+    const { globalState, setGlobalState } = useContext(Store);
+
+    useEffect(() => {
+        fetchPopularData().then((res) => {
+            console.log('data',res);
+            setGlobalState({ type: 'SET_POPULAR', payload: {popular: res.data.items}})
+        })
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, []);
+ 
+    return(
+        <Layout>
+            top
+        </Layout>
+    );
+}
+
+export default Top;
